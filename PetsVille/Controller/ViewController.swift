d347a50e0e1212f723487bdc5d5caf57eb041600
Vadim <ivo.dev.ios@gmail.com>
2022-05-23 22:15:18 +0300

    s

--- PetsVille/Controller/ViewController.swift
+++ PetsVille/Controller/ViewController.swift
@@ -1,19 +1,8 @@
-//
-//  ViewController.swift
-//  PetsVille
-//
-//  Created by Vadim on 23.05.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
